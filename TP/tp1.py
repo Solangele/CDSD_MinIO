3f1e217cf60993ba09177c1c60bc9309ecf3a085
Solange Devos <angedespretz@gmail.com>
2025-11-12 11:50:47 +0100

    Tp1 terminé

--- TP/tp1.py
+++ TP/tp1.py
@@ -1,6 +1,8 @@
 from minio import Minio
 from minio.error import S3Error
 from minio.commonconfig import CopySource
+from minio.deleteobjects import DeleteObject
+import os
 
 
 def main():
@@ -70,8 +72,39 @@ def main():
 
 
     # 8 : Téléchargement d'un objet
-    
+    local_download_path = "./telecharge-test-tp1.txt"
+    client.fget_object(
+        bucket_name=bucket_name,
+        object_name=new_name,
+        file_path=local_download_path,
+    )
+    print(f"\nFichier '{new_name}' téléchargé localement sous '{local_download_path}'.")
 
+
+    # 9 : suppression d'un objet
+    print("Suppression d’objets dans le bucket...")
+
+    # Suppression d’un seul objet :
+    client.remove_object(bucket_name, destination_file)
+    print(f"- '{destination_file}' supprimé.")
+
+    # Suppression multiple :
+    objects_to_delete = [new_name]
+    objects_to_delete = [DeleteObject("renomme-test-tp1.txt")]
+    for error in client.remove_objects(bucket_name, objects_to_delete):
+        print("Erreur lors de la suppression :", error)
+
+
+    # 10 : Nettoyage local
+    print("Nettoyage des fichiers locaux...")
+    for fichier in [source_file, local_download_path]:
+        if os.path.exists(fichier):
+            os.remove(fichier)
+            print(f"- {fichier} supprimé.")
+        else:
+            print(f"- {fichier} n'existe pas (déjà supprimé ou jamais créé).")
+
+    print("\n✅ Fin du script : tout est propre !")
 
 
 
